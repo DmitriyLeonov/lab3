f40d375084e9988e93c39483abeeffc1010c0174
DmitryLeonov <dleonov701@gmail.com>
2021-09-29 16:29:26 +0300

    task2,3

--- Program.cs
+++ Program.cs
@@ -4,8 +4,60 @@ namespace lab3
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            House house1 = new House();
+            House house2 = new House(28);
+            House house3 = new House(25, 42.4f, 3, 2, "Пушкина", "панельный", 40);
+            int o = 0, number = 23, numberOfRooms = 0, minFloor = 0, maxFloor = 0;
+            house1.Number(ref number, out o);
+
+            Console.WriteLine(house1.ToString());
+            Console.WriteLine(house3.GetHashCode());
+            Console.WriteLine(house1.Equals(house2));
+            Console.WriteLine("{0}\n{1}", house3.BuildingType, house3.Floor);
+
+            House[] houses = new House[5] {house3,
+                new House(2, 32f, 4, 1, "Пушкина", "панельный", 40),
+                new House(5, 44.2f, 2, 2, "Ленина", "панельный", 60),
+                new House(55, 62.4f, 1, 3, "Сурганова", "панельный", 50),
+                new House(22, 142.4f, 5, 5, "Кирова", "кирпичный", 77)
+            };
+            numberOfRooms = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            foreach(House house in houses)
+            {
+                if(house.NumberOfRooms == numberOfRooms)
+                {
+                    Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}",
+                        house.FlatNumber, house.Square, house.Floor, house.NumberOfRooms,
+                        house.Street, house.BuildingType, house.Lifetime, house.BuildYear);
+                    Console.WriteLine(house.IsRepairNeeded());
+                    Console.WriteLine("---------------------------");
+                }
+            }
+            numberOfRooms = Convert.ToInt32(Console.ReadLine());
+            minFloor = Convert.ToInt32(Console.ReadLine());
+            maxFloor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            foreach (House house in houses)
+            {
+                if (house.NumberOfRooms == numberOfRooms && house.Floor >= minFloor && house.Floor <= maxFloor)
+                {
+                    Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}",
+                        house.FlatNumber, house.Square, house.Floor, house.NumberOfRooms,
+                        house.Street, house.BuildingType, house.Lifetime, house.BuildYear);
+                    Console.WriteLine("---------------------------");
+                }
+            }
+
+            var house4 = new { FlatNumber = 25, Square = 42.4f, Floor = 3, NumberOfRooms = 2,
+                Street = "Пушкина",BuildingType =  "панельный", Lifetime = 45 };
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}",
+                       house4.FlatNumber, house4.Square, house4.Floor, house4.NumberOfRooms,
+                       house4.Street, house4.BuildingType, house4.Lifetime);
+
+            Console.Read();
         }
     }
 
@@ -14,23 +66,105 @@ namespace lab3
         private static int counter;
         private const int currentYear = 2021;
 
+        private int flatnumber;
+        private float square;
+        private int floor;
+        private int numberofrooms;
+        private string street;
+        private string buildingtype;
+        private int lifetime;
+
+
         public int Id { get; }
-        public int FlatNumber { get; set; }
-        public float Square { get; set; }
-        public int Floor { get; set; }
-        public int NumberOfRooms { get; set; }
-        public string Street { get; set; }
-        public string BuildingType { get; set; }
-        public int Lifetime { get; set; }
+        public int FlatNumber { 
+            get { return flatnumber; }
+            set {
+                if (value <= 0)
+                    Console.WriteLine("Номер квартиры должен быть больше 0");
+                else
+                    flatnumber = value;
+            }
+        }
+        public float Square
+        {
+            get { return square; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Площадь должена быть больше 0");
+                else
+                    square = value;
+            }
+        }
+        public int Floor
+        {
+            get { return floor; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Этаж должен быть больше 0");
+                else
+                    floor = value;
+            }
+        }
+        public int NumberOfRooms
+        {
+            get { return numberofrooms; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Количество комнат должено быть больше 0");
+                else
+                    numberofrooms = value;
+            }
+        }
+        public string Street
+        {
+            get { return street; }
+            set
+            {
+                if (value == null)
+                    Console.WriteLine("Улица не болжна быть null");
+                else
+                    street = value;
+            }
+        }
+        public string BuildingType
+        {
+            get { return buildingtype; }
+            set
+            {
+                if (value == null)
+                    Console.WriteLine("Тип здания не болжен быть null");
+                else
+                    buildingtype = value;
+            }
+        }
+        public int Lifetime
+        {
+            get { return lifetime; }
+            set
+            {
+                if (value <= 0)
+                    Console.WriteLine("Срок эксплуатации должен быть больше 0");
+                else
+                    lifetime = value;
+            }
+        }
+        public int BuildYear { get; }
+
+        Random rnd = new Random();
 
         public House()
         {
             counter++;
             Id = counter;
+            Lifetime = 40;
+            BuildYear = rnd.Next(1950, 2000);
         }
 
         public House(int flatNumber, float square, int floor, int numberOfRooms,
-            string street, string buildingType, int lifetime)
+                     string street, string buildingType,int lifetime)
         {
             counter++;
             Id = counter;
@@ -41,13 +175,18 @@ namespace lab3
             Street = street;
             BuildingType = buildingType;
             Lifetime = lifetime;
+            Lifetime = 40;
+            BuildYear = rnd.Next(1950, 2000);
         }
 
-        public House(string street = "Свердлова")
+        public House( int flatNumber , string street = "Свердлова")
         {
             counter++;
             Id = counter;
+            FlatNumber = flatNumber;
             Street = street;
+            Lifetime = 40;
+            BuildYear = rnd.Next(1950, 2000);
         }
 
         static House()
@@ -60,10 +199,13 @@ namespace lab3
             counter++;
             Id = counter;
             FlatNumber = 45;
+            Lifetime = 40;
+            BuildYear = rnd.Next(1950, 2000);
         }
 
         public void Number( ref int number, out int o)
         {
+            number = number + 15;
             new House(number);
             o = 20;
         }
@@ -71,6 +213,13 @@ namespace lab3
         public static void HouseInfo()
         {
             Console.WriteLine(typeof(House));
+        }
+
+        public bool IsRepairNeeded()
+        {
+            if ((currentYear - this.BuildYear) > this.Lifetime)
+                return true;
+            return false;
         }
 
         public override bool Equals(object obj)
